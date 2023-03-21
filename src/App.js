ced133b5940599c18f71daf6028437bb9c1c4ae6
sarah jedlaoui <sarajedlaoui999@gmail.com>
2023-03-21 09:49:44 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  
-  Route,
- 
-  Navigate,
-  
-} from "react-router-dom";
-import { Provider } from "react-redux";
-//import CookieConsent from 'react-cookie-consent';
-import { Routes } from 'react-router-dom';
-import { connect } from "react-redux";
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Provider, connect } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
 import { store } from "./store";
-
 import Home from "./components/Home";
 
-
-
-const withRouter = Component => props => {
+function App(props) {
   const location = useLocation();
   const navigate = useNavigate();
-  const params = useParams();
 
   return (
-    <Component
-      {...props}
-      location={location}
-      navigate={navigate}
-      params={params}
-    />
+    <Provider store={store}>
+      <Routes location={location} navigate={navigate}>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Provider>
   );
-};
-
-
-class App extends Component {
-
-  render() {
-
-    return (
-      <Provider store={store}>
-        <Router>
-          <Routes>
-             <Navigate from="*" to="/" />
-             <Route exact path="/" element={Home} />
-           
-          </Routes>
-        </Router>
-        </Provider>
-    );
-  }
 }
 
 const mapStateToProps = (state) => ({
@@ -58,5 +24,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-
-export default withRouter(connect(mapStateToProps)(App));
+export default connect(mapStateToProps)(App);
