861f75361f3f8eea9037f6bfc218f5a118b7a849
SarahJedlaoui <69294734+SarahJedlaoui@users.noreply.github.com>
2023-05-23 15:09:38 +0200

    first commit

--- src/components/Certificate/certificat.js
+++ src/components/Certificate/certificat.js
@@ -2,17 +2,220 @@ import React, { useEffect, useState } from 'react';
 
 import  { useLocation,useParams } from 'react-router-dom';
 import Certificate from './index';
+import {
+  FacebookShareCount,
+  RedditShareCount,
+  TumblrShareCount,
+  FacebookShareButton,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  LinkedinShareButton,
+  TwitterShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  RedditShareButton,
+  EmailShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  LineShareButton,
+  WeiboShareButton,
+  PocketShareButton,
+  InstapaperShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  RedditIcon,
+  TumblrIcon,
+  MailruIcon,
+  EmailIcon,
+  LivejournalIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  LineIcon,
+  PocketIcon,
+  InstapaperIcon,
+  WeiboIcon,
+} from "react-share";
+
+
+
+
+
 
 function Certificat() {
   const {certificateNumber}=useParams();
+  const title = "E certification "
   const location = useLocation();
   const cid = new URLSearchParams(location.search).get("cid");
   const distributorPublicKey = new URLSearchParams(location.search).get("distributorPublicKey");
   const issuerPublicKey = new URLSearchParams(location.search).get("issuerPublicKey");
   const stellarLab = "https://horizon-futurenet.stellar.org/accounts/?sponsor=GC4MEJJJMNIBIDZSJOZOPVUQQUKR3AARFLPFYKUFXU2D7PHWJP5S4AEI"
+  const shareUrl = `https://edunode.org/certificates/${certificateNumber}?cid=${cid}&distributorPublicKey=${distributorPublicKey}&issuerPublicKey=${issuerPublicKey}`;
   return (
     <div>
-    
+    <div className="Demo__container">
+                <div className="Demo__some-network">
+                  <FacebookShareButton
+                    url={shareUrl}
+                    quote={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <FacebookIcon size={32} round />
+                  </FacebookShareButton>
+
+                  <div>
+                    <FacebookShareCount
+                      url={shareUrl}
+                      className="Demo__some-network__share-count"
+                    >
+                      {(count) => count}
+                    </FacebookShareCount>
+                  </div>
+                </div>
+
+                <div className="Demo__some-network">
+                  <FacebookMessengerShareButton
+                    url={shareUrl}
+                    appId="521270401588372"
+                    className="Demo__some-network__share-button"
+                  >
+                    <FacebookMessengerIcon size={32} round />
+                  </FacebookMessengerShareButton>
+                </div>
+
+                <div className="Demo__some-network">
+                  <TwitterShareButton
+                    url={shareUrl}
+                    title={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
+
+                  <div className="Demo__some-network__share-count">
+                    &nbsp;
+                  </div>
+                </div>
+
+                <div className="Demo__some-network">
+                  <TelegramShareButton
+                    url={shareUrl}
+                    title={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <TelegramIcon size={32} round />
+                  </TelegramShareButton>
+
+                  <div className="Demo__some-network__share-count">
+                    &nbsp;
+                  </div>
+                </div>
+
+                <div className="Demo__some-network">
+                  <WhatsappShareButton
+                    url={shareUrl}
+                    title={title}
+                    separator=":: "
+                    className="Demo__some-network__share-button"
+                  >
+                    <WhatsappIcon size={32} round />
+                  </WhatsappShareButton>
+
+                  <div className="Demo__some-network__share-count">
+                    &nbsp;
+                  </div>
+                </div>
+
+                <div className="Demo__some-network">
+                  <LinkedinShareButton
+                    url={shareUrl}
+                    className="Demo__some-network__share-button"
+                  >
+                    <LinkedinIcon size={32} round />
+                  </LinkedinShareButton>
+                </div>
+
+                <div className="Demo__some-network">
+                  <RedditShareButton
+                    url={shareUrl}
+                    title={title}
+                    windowWidth={660}
+                    windowHeight={460}
+                    className="Demo__some-network__share-button"
+                  >
+                    <RedditIcon size={32} round />
+                  </RedditShareButton>
+
+                  <div>
+                    <RedditShareCount
+                      url={shareUrl}
+                      className="Demo__some-network__share-count"
+                    />
+                  </div>
+                </div>
+
+                <div className="Demo__some-network">
+                  <TumblrShareButton
+                    url={shareUrl}
+                    title={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <TumblrIcon size={32} round />
+                  </TumblrShareButton>
+                </div>
+
+                <div className="Demo__some-network">
+                  <EmailShareButton
+                    url={shareUrl}
+                    subject={title}
+                    body="body"
+                    className="Demo__some-network__share-button"
+                  >
+                    <EmailIcon size={32} round />
+                  </EmailShareButton>
+                </div>
+                <div className="Demo__some-network">
+                  <ViberShareButton
+                    url={shareUrl}
+                    title={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <ViberIcon size={32} round />
+                  </ViberShareButton>
+                </div>
+
+                <div className="Demo__some-network">
+                  <WorkplaceShareButton
+                    url={shareUrl}
+                    quote={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <WorkplaceIcon size={32} round />
+                  </WorkplaceShareButton>
+                </div>
+
+                <div className="Demo__some-network">
+                  <LineShareButton
+                    url={shareUrl}
+                    title={title}
+                    className="Demo__some-network__share-button"
+                  >
+                    <LineIcon size={32} round />
+                  </LineShareButton>
+                </div>
+
+              </div>
   
      <img src={cid} alt="Certificate" />
      <h4>certificate Id:{certificateNumber}</h4>
